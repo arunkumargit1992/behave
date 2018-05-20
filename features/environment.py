cd916a890b1499530e1e93416742419b1856ffa0
Arunkumar <letterbox.arunkumar.1992@gmail.com>
2018-05-20 21:52:44 +0530

    modified

--- features/environment.py
+++ features/environment.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 def before_all(context):
     context.browser = webdriver.Chrome("E://chromedriver.exe")
     # context.browser = webdriver.Chrome() if you have set chromedriver in your PATH
-    context.browser.set_page_load_timeout(10)
-    context.browser.implicitly_wait(10)
+    context.browser.set_page_load_timeout(50)
+    context.browser.implicitly_wait(30)
     context.browser.maximize_window()
 
 def after_all(context):
--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -6,12 +6,15 @@ import time
 
 @step('I navigate to the facebook Home page')
 def step_impl(context):
-    context.browser.get("https://www.facebook.com/") 
-    time.sleep(5)   
-	  
+    context.browser.get("https://www.facebook.com/")
+    time.sleep(2)
+    
+    
+     
 @step('I enter username password and click login button')
 def step_impl(context):
     context.browser.find_element(By.ID, "email").send_keys("behavetest10@gmail.com")    
+    time.sleep(2)
     context.browser.find_element(By.ID, "pass").send_keys("behave123!@#")
     context.browser.find_element(By.ID, "u_0_2").click()
     
